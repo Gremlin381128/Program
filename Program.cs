d26c969865b560df42cb3789e21f78c47e0aabe4
Gremlin381128 <arturkobialka2@gmail.com>
2021-03-02 10:51:40 +0100

    Dodanie wartości a, b oraz sumy c

--- Program.cs
+++ Program.cs
@@ -6,7 +6,10 @@ namespace Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int a = 42;
+            int b = 119;
+            int c = a + b;
+            Console.WriteLine(c);
         }
     }
 }
