64755bdc6755d563e0433db10cf66c1cc5f96eb2
Gremlin381128 <arturkobialka2@gmail.com>
2021-05-04 17:44:44 +0200

    Update menu głównego aplikacji (5 opcji do wyboru)

--- Program.cs
+++ Program.cs
@@ -1,18 +1,54 @@
 ﻿using System;
 
-namespace Program
+namespace Yes
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.Clear();
-            Console.WriteLine("Menu główne");
-            Console.WriteLine("1. Dodaj książkę");
-            Console.WriteLine("2. Znajdź książkę po tytule");
-            Console.WriteLine("3. Znajdź książkę po imieniu i nazwisku autora");
-            Console.Write("\r\nWybierz opcję: ");
-            Console.ReadLine();
+            bool logLoop = true;
+            while (logLoop == true)
+            {
+                try
+                {
+                    Console.WriteLine("1) Dodaj książkę");
+                    Console.WriteLine("2) Znajdź książki po tytule");
+                    Console.WriteLine("3) Znajdź książki po imieniu i nazwisku autora");
+                    Console.WriteLine("4) Wyświetl wszystkie książki");
+                    Console.WriteLine("5) Wyjdź z aplikacji");
+
+                    int logCase = int.Parse(Console.ReadLine());
+
+                    switch (logCase)
+                    {
+                        case 1:
+                            
+                            break;
+                        case 2:
+                            
+                            break;
+                        case 3:
+                            
+                            break;
+                        case 4:
+                            
+                            break;
+                        case 5:
+                            logLoop = false;
+                            break;
+                        default:
+                            Console.WriteLine("Wprowadź poprawną wartość...");
+                            break;
+                    }
+                }
+                catch (FormatException)
+                    {
+                        logLoop = true;
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Spróbuj ponownie...");
+                        Console.ForegroundColor = ConsoleColor.White;
+                    }
+                }
         }
     }
 }
